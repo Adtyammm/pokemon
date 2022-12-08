6866240216891065d839ee79f3ca1de802d645bc
Aditya Muhamad Maulana <yadit16262@gmail.com>
2022-12-08 17:35:01 +0700

    Updates

---
+++ precache-manifest.8a0e64af1f88224069d2b16511d9a2fb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9cad3c8b3ecb8e5cbda",
+    "url": "/pokemon/static/js/main.c9cad3c8.chunk.js"
+  },
+  {
+    "revision": "5e7ac3bcfe842a2bc07a",
+    "url": "/pokemon/static/js/1.5e7ac3bc.chunk.js"
+  },
+  {
+    "revision": "c935f654a987b9da8bd7",
+    "url": "/pokemon/static/js/runtime~main.c935f654.js"
+  },
+  {
+    "revision": "8f44c5fb3d1bad335d480642093533b1",
+    "url": "/pokemon/index.html"
+  }
+];
